1bd6ae61e22ac834c8671a9dcccfc79af7e3bda0
maruf-pfc <mdmarufsarker.mms@gmail.com>
2026-05-08 02:33:40 +0600

    feat: installed tailwindcss for client and necessary packages for server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <h1>StoreDesk Inventory System</h1>
+      <div className="bg-green-600 h-screen flex items-center justify-center">
+        <h1 className="text-center text-3xl font-bold">
+          StoreDesk Inventory System
+        </h1>
+      </div>
     </>
   );
 }
