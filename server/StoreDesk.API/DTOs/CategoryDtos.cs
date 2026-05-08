de53224fce5451f1e174c4374a79b0411443d937
maruf-pfc <mdmarufsarker.mms@gmail.com>
2026-05-08 22:55:02 +0600

    feat: add category DTO validation

--- server/StoreDesk.API/DTOs/CategoryDtos.cs
+++ server/StoreDesk.API/DTOs/CategoryDtos.cs
@@ -1,14 +1,23 @@
 namespace StoreDesk.API.DTOs;
+using System.ComponentModel.DataAnnotations;
 
 public class CreateCategoryDto
 {
+    [Required]
+    [MaxLength(100)]
     public string Name { get; set; } = string.Empty;
+
+    [MaxLength(500)]
     public string? Description { get; set; }
 }
 
 public class UpdateCategoryDto
 {
+    [Required]
+    [MaxLength(100)]
     public string Name { get; set; } = string.Empty;
+
+    [MaxLength(500)]
     public string? Description { get; set; }
 }
 
