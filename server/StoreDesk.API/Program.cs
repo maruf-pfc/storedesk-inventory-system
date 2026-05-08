7505257d5e8c8b97540ff3d5701024fcd280f5cf
maruf-pfc <mdmarufsarker.mms@gmail.com>
2026-05-09 00:57:41 +0600

    feat: standardize validation error responses

--- server/StoreDesk.API/Program.cs
+++ server/StoreDesk.API/Program.cs
@@ -3,12 +3,33 @@ using StoreDesk.API.Data;
 using StoreDesk.API.Interfaces;
 using StoreDesk.API.Services;
 using StoreDesk.API.Middleware;
+using Microsoft.AspNetCore.Mvc;
+using StoreDesk.API.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            var errors = context.ModelState
+                .Values
+                .SelectMany(value => value.Errors)
+                .Select(error => error.ErrorMessage)
+                .ToList();
+
+            var response =
+                ApiResponse<string>.FailureResponse(
+                    "Validation failed",
+                    errors);
+
+            return new BadRequestObjectResult(response);
+        };
+    });
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
