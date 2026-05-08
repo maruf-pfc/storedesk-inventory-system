20841a00582a8823c5436874153d963af8b14867
maruf-pfc <mdmarufsarker.mms@gmail.com>
2026-05-08 16:45:55 +0600

    feat: Configure PostgreSQL and add initial migration

---
+++ server/StoreDesk.API/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using StoreDesk.API.Models;
+
+namespace StoreDesk.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+    public DbSet<Category> Categories => Set<Category>();
+}
